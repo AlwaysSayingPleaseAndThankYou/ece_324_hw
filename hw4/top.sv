368007ff19f3a563249e1cc13fc905b743bc6a3a
Gus <aengus.crowley@gmail.com>
2020-02-11 15:44:35 -0800

    init

---
+++ hw4/top.sv
@@ -0,0 +1,15 @@
+module top_module (
+	input [1:0] s,
+	input [3:0] a,
+	input [3:0] b,
+	output [3:0] y
+	);
+
+always@(*) begin
+	case(s):
+		2'b00: y = a + b;
+		2'b01: y = a << b[1:0];
+		2'b10: y = a & b;
+		2'b11: y = 4'b0001;
+
+		end
