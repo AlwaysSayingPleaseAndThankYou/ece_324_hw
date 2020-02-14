1a7157a6795224c43725838d7c1dbd50bb29835d
Gus <aengus.crowley@gmail.com>
2020-02-13 17:06:25 -0800

    added assert

--- hw3/basic_aluTB.sv
+++ hw3/basic_aluTB.sv
@@ -12,8 +12,6 @@ int _s = -1;
 int _a = -1;
 int _b = -1;
 //where does the assert go?
-	//assert((y == a[3:0] + b[3:0]) & (s ==0))
-		//else $error("FAIL: opcode is add(00), input is %b and %b, output is %b",a,b, y);
 	initial begin
 			for (int i = 0; i <= 3; i++) begin
 				_s++;
@@ -21,6 +19,8 @@ int _b = -1;
 					_a++;
 					for (int k = 0; k <= 15; k++) begin
 						_b++;
+						assert((y == a[3:0] + b[3:0]) & (s ==0))
+							else $error("FAIL: opcode is add(00), input is %b and %b, output is %b",a,b, y);
                         #10 s=_s; a=_a; b=_b;
                     end
 				end
---
+++ hw3/gus-tb.sv
@@ -0,0 +1,31 @@
+`timescale 1ns/10ps
+module top ();
+	logic [1:0] s;
+	logic [3:0] a;
+	logic [3:0] b;
+	logic [3:0] y;
+
+//positional assign rather than name delcaration
+	top UUT (s,a,b,y);
+	int _s = 0;
+	int _a = 0;
+	int _b = 0;
+//where does the assert go?
+	assert((y == a[3:0] + b[3:0]) & (s ==0))
+		else $error("FAIL: opcode is add(00), input is %b and %b, output is %b",a[3:0],b[3:0], y);
+	initial begin
+			for (int i = 0; i <= 3; i++) begin
+				_s ++;
+				for (int j = 0; j <= 15; j++) begin
+					_a++
+						for (int k = 0; k <= 15; k++) begin
+							_b++
+								//can I assign int to logic?
+								// this should be fine
+								#10 s = _s, a=_a, b=_b;
+						end
+				end
+			end
+		#10 $stop;
+	end
+endmodule
