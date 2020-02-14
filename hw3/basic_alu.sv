caa6010ab8a31144ab7d2836207be00840de3f49
Crowley <aengus.crowley@wsu.edu>
2020-02-13 16:00:02 -0800

    proof added

--- hw3/basic_alu.sv
+++ hw3/basic_alu.sv
@@ -4,12 +4,12 @@ module basic_alu(s,a,b,y);
 	input logic [3:0] b;
 	output logic [3:0] y;
 
-always@(*)
+always@(*) begin
 	case(s)
 		2'b00: y = a + b;
 		2'b01: y = a << b[1:0];
 		2'b10: y = a & b;
 		2'b11: y = 1;	
-
-		
+	endcase
+end
 endmodule		
--- hw3/basic_aluTB.sv
+++ hw3/basic_aluTB.sv
@@ -5,25 +5,24 @@ module basic_aluTB;
 	logic [3:0] b;
 	logic [3:0] y;
 
-basic_alu UUT(.s(s),.a(a),.b(b),.y(y));
+//basic_alu UUT(.s(s),.a(a),.b(b),.y(y));
+basic_alu UUT(s,a,b,y);
 
-int _s = 0;
-int _a = 0;
-int _b = 0;
+int _s = -1;
+int _a = -1;
+int _b = -1;
 //where does the assert go?
 	//assert((y == a[3:0] + b[3:0]) & (s ==0))
 		//else $error("FAIL: opcode is add(00), input is %b and %b, output is %b",a,b, y);
 	initial begin
 			for (int i = 0; i <= 3; i++) begin
-				_s ++;
+				_s++;
 				for (int j = 0; j <= 15; j++) begin
 					_a++;
-						for (int k = 0; k <= 15; k++) begin
-							_b++;
-								//can I assign int to logic?
-								// this should be fine
-								#10 s = _s; a=_a; b=_b;
-						end
+					for (int k = 0; k <= 15; k++) begin
+						_b++;
+                        #10 s=_s; a=_a; b=_b;
+                    end
 				end
 			end
 		#10 $stop;
